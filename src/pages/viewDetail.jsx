4cdc641bc0949e986d587588bb22a2514d46d4d5
Abhishek <133025200+abhishekvithani2811@users.noreply.github.com>
2023-11-01 10:01:24 +0530

    Update viewDetail.jsx

--- src/pages/viewDetail.jsx
+++ src/pages/viewDetail.jsx
@@ -13,7 +13,7 @@ import Modal from '@mui/material/Modal';
 
 const style = {
     position: 'absolute',
-    top: '50%',
+    top: '50%' ,
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
